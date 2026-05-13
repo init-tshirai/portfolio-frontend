86e0b8728c0c82de187b6bd9ba3361d8a7075350
shirait <hamburgkarry@gmail.com>
2026-05-13 17:48:37 +0900

    Home画面にログアウトボタンを配置（場所は仮）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import LogoutButton from "./components/LogoutButton"
+
+/*
 type Profile = {
   name: string
   role: string
   message: string
 }
+  */
 
 export default async function HomePage() {
+  /*
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/profile`,
     {
       cache: "no-store",
@@ -20,4 +25,12 @@ export default async function HomePage() {
       <p>{profile.message}</p>
     </main>
   );
+  */
+
+  return (
+    <main>
+      <h1>Home</h1>
+      <LogoutButton />
+    </main>
+  )
 }
