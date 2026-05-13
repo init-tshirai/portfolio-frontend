3661c6979757f50bff8514a49a7098e5392993d0
shirait <hamburgkarry@gmail.com>
2026-05-13 11:04:48 +0900

    RailsAPIから取得したJSONを表示するサンプルを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
 import Image from "next/image";
 
-export default function Home() {
+type Profile = {
+  name: string
+  role: string
+  message: string
+}
+
+export default async function HomePage() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/profile`,
+    {
+      cache: "no-store",
+    }
+  )
+
+  const profile: Profile = await res.json()
+
   return (
     <main>
-      <h1>Home</h1>
-      <p>ポートフォリオサイトへようこそ。</p>
+      <h1>{profile.name}</h1>
+      <p>{profile.role}</p>
+      <p>{profile.message}</p>
     </main>
   );
 }
