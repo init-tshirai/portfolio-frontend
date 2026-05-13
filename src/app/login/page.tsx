beb5e1e67eba1f9ab3e05945564e60eb693451a6
shirait <hamburgkarry@gmail.com>
2026-05-13 15:19:09 +0900

    ログイン画面を追加途中

---
+++ src/app/login/page.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import React, { useState } from "react"
+
+export default function LoginPage() {
+  const router = useRouter()
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [errorMessage, setErrorMessage] = useState("")
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()  // formのsubmitを押したとき、ページがリロードされることを防ぐ
+
+    setErrorMessage("")
+    setIsSubmitting(true)
+
+    const res = await fetch("api/autho/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+
+    setIsSubmitting(false)
+
+    if(!res.ok) {
+      // review: エラーメッセージの内容について。サーバから受け取った内容をそのまま表示してよいのか。
+      const data = await res.json().catch(() => null)
+      setErrorMessage(data?.message ?? "ログインに失敗しました。")
+      return
+    }
+
+    router.push("/")
+    router.refresh()
+  }
+
+  return (
+    <main>
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit}>
+
+      </form>
+    </main>
+
+  )
+}
